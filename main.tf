b68e723245c6a2fb52deb707ca11746042dff001
Lin <linb1@plvinetnmondb1.mskcc.org>
2025-06-02 16:17:44 -0400

    Create Datadog dashboard via Terraform

--- main.tf
+++ main.tf
@@ -1,15 +1,50 @@
 terraform {
   required_providers {
-    null = {
-      source = "hashicorp/null"
-      version = "~> 3.0"
+    datadog = {
+      source  = "DataDog/datadog"
+      version = "~> 3.30.0"
     }
   }
 }
 
-resource "null_resource" "example" {
-  provisioner "local-exec" {
-    command = "echo Hello from Terraform!"
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  site    = var.datadog_site
+}
+
+variable "datadog_api_key" {}
+variable "datadog_app_key" {}
+variable "datadog_site" {}
+
+resource "datadog_dashboard" "example_dashboard" {
+  title       = "TLM PoC Dashboard"
+  description = "My test dashboard created via Terraform and GitHub Actions"
+  layout_type = "ordered"
+  is_read_only = false
+
+  widget {
+    group_definition {
+      title = "Sample Group"
+      layout_type = "ordered"
+      widget {
+        timeseries_definition {
+          title = "System Load (avg. 1m)"
+          show_legend = true
+          legend_size = "0"
+          type = "line"
+          requests {
+            q = "avg:system.load.1{*}"
+            display_type = "line"
+            style {
+              palette = "dog_classic"
+              line_type = "solid"
+              line_width = "normal"
+            }
+          }
+        }
+      }
+    }
   }
 }
 
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_app_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_site" {
+  type    = string
+  default = "datadoghq.com"
+}
+
