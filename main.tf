b355845e174bcf66b0614db3a58e03a9e267a128
Lin <linb1@plvinetnmondb1.mskcc.org>
2025-06-02 11:07:39 -0400

    Initial commit of terraform AWS infra

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  provisioner "local-exec" {
+    command = "echo Hello from Terraform!"
+  }
+}
+
