6bc6f0430eb2cdf46eda3142351a1910caad760b
Lin <linb1@plvinetnmondb1.mskcc.org>
2025-06-02 16:24:57 -0400

    Var Datadog dashboard via Terraform

--- main.tf
+++ main.tf
@@ -1,50 +1,26 @@
-terraform {
-  required_providers {
-    datadog = {
-      source  = "DataDog/datadog"
-      version = "~> 3.30.0"
-    }
-  }
-}
-
+# main.tf — good!
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
   site    = var.datadog_site
 }
 
-variable "datadog_api_key" {}
-variable "datadog_app_key" {}
-variable "datadog_site" {}
+# variables.tf — this is where they should be defined
+variable "datadog_api_key" {
+  description = "Datadog API key"
+  type        = string
+  sensitive   = true
+}
 
-resource "datadog_dashboard" "example_dashboard" {
-  title       = "TLM PoC Dashboard"
-  description = "My test dashboard created via Terraform and GitHub Actions"
-  layout_type = "ordered"
-  is_read_only = false
+variable "datadog_app_key" {
+  description = "Datadog APP key"
+  type        = string
+  sensitive   = true
+}
 
-  widget {
-    group_definition {
-      title = "Sample Group"
-      layout_type = "ordered"
-      widget {
-        timeseries_definition {
-          title = "System Load (avg. 1m)"
-          show_legend = true
-          legend_size = "0"
-          type = "line"
-          requests {
-            q = "avg:system.load.1{*}"
-            display_type = "line"
-            style {
-              palette = "dog_classic"
-              line_type = "solid"
-              line_width = "normal"
-            }
-          }
-        }
-      }
-    }
-  }
+variable "datadog_site" {
+  description = "Datadog site URL"
+  type        = string
+  default     = "datadoghq.com"
 }
 
