9bc5bf3ebdbefe14925eddbad6d03d23b0fe5edf
Dennis <47396354+dennisbomb@users.noreply.github.com>
2025-06-03 11:55:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ terraform {
   required_providers {
     datadog = {
       source  = "DataDog/datadog"
-      version = "~> 3.30"
+      version = "3.27.0"
     }
   }
 }
+
 
 provider "datadog" {
   api_key = var.datadog_api_key
@@ -14,15 +15,18 @@ provider "datadog" {
 }
 
 resource "datadog_monitor" "cpu_high" {
-  name    = "High CPU Usage (Working Example)"
-  type    = "query alert"
+  name = "High CPU Alert"
+  type = "query alert"
+  query = "avg(last_5m):avg:system.cpu.user{*} > 90"
 
-  query   = "avg(last_5m):avg:system.cpu.user{*} > 90"
+  message = "🚨 CPU usage is high on {{host.name}}"
+  tags    = ["env:test"]
 
-  message = "🚨 High CPU detected on {{host.name}}"
-  tags    = ["env:test", "team:ops"]
+  thresholds {
+    critical = 90
+  }
 
-  threshold_critical = 90
-  notify_no_data     = true
-  no_data_timeframe  = 10
+  notify_no_data    = true
+  no_data_timeframe = 10
 }
+
